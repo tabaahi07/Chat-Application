35b57764d30beeda8f08013ad76f69ceefcd48fa
tabaahi07 <126075036+tabaahi07@users.noreply.github.com>
2023-07-27 22:15:28 +0530

    Done! InputBox

--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -3,7 +3,13 @@ import styles from "../CSS/InputBox.module.css";
 
 export default function InputBox() {
   return <div className={styles.InputBox}>
-       
+       <div className={styles.InputMessage} >
+           <input type="text" placeholder="Enter a message" />
+       </div>
+
+       <div className={styles.SendBtn} >
+          <button > Send </button>
+       </div>
 
   </div>;
 }
