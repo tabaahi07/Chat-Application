72b581c0392655d1a2f95c015331bf8fa78549ec
Rahul Goel <211220042@nitdelhi.ac.in>
2023-07-27 23:28:29 +0530

    server init

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
