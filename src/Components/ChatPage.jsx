3458a5a3dc8fb75943f8baef8351a72f5c6ade69
Rahul Goel <211220042@nitdelhi.ac.in>
2023-08-03 13:53:55 +0530

    libraries install

--- src/Components/ChatPage.jsx
+++ src/Components/ChatPage.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import MessageBox from './MessageBox';
 import BottomPanel from './BottomPanel';
 import { UserState } from '../Context/UserContext';
+import io from "socket.io-client"
 
 const ChatPage = () => {
     const [socket , setSocket] = useState("") ;
